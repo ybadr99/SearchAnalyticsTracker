45cc06a4ec34a2276b2718efd9935df752d144d1
ybadr99 <yousefbadr909@gmail.com>
2024-01-12 16:20:32 +0200

    using js and ajax

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,14 @@ class SearchController < ApplicationController
   def search
     @results = Article.search(params[:search])
 
-    render turbo_stream: turbo_stream.update('articles', partial: 'articles/articles',
-                                                         locals: { articles: @results })
+    @results = Article.search('*') if params[:search].blank?
+
+    respond_to do |format|
+      # format.turbo_stream do
+      #   render turbo_stream: turbo_stream.update('articles', partial: 'articles/articles',
+      #                                                        locals: { articles: @results })
+      # end
+      format.json { render json: @results } # JSON response
+    end
   end
 end
