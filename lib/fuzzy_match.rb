845f83faa3bc435029fbfbf15fcc455f3e3211b0
ybadr99 <yousefbadr909@gmail.com>
2024-01-18 01:45:36 +0200

    update the fuzzy matcher class

--- lib/fuzzy_match.rb
+++ lib/fuzzy_match.rb
@@ -1,29 +1,28 @@
-# frozen_string_literal: true
-
 module FuzzyMatch
   class FuzzyMatcher
-    def initialize(collection, options = {})
+    def initialize(collection)
       @collection = collection
-      @attribute = options[:read]
     end
 
+    # calculate similarity and bigram scores 
     def calculate_levenshtein_distance(str1, str2)
       bigrams1 = str1.downcase.each_char.each_cons(2).map(&:join)
       bigrams2 = str2.downcase.each_char.each_cons(2).map(&:join)
-
-      common_bigrams = (bigrams1 & bigrams2).uniq
-      total_bigrams = (bigrams1 | bigrams2).uniq
+      #  intersection of the two bigram arrays
+      common_bigrams = bigrams1 & bigrams2
+      # union of the two bigram arrays
+      total_bigrams = bigrams1 | bigrams2
 
       common_bigrams.length.to_f / total_bigrams.length
     end
 
-    def fuzzy_match(query, threshold = 0.3)
+    def fuzzy_match(query)
       query = query.downcase
       best_match = nil
-      max_similarity = threshold
+      max_similarity = 0
 
       @collection.each do |item|
-        value = @attribute ? item[@attribute].to_s.downcase : item.to_s.downcase
+        value = item.to_s.downcase
 
         similarity = calculate_levenshtein_distance(value, query)
 
